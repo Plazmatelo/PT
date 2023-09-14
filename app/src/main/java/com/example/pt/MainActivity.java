6f78ae585e7165997a411714378815d48f6109d7
plazmatelo <plazmatelo@gmail.com>
2023-09-14 12:20:59 +0200

    super nie dziala

---
+++ app/src/main/java/com/example/pt/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            }
+        }
+    }
+}
